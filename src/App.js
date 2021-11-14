c655e43a625aa47a8bee0a0892fe3416046b56e0
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-11-14 16:38:53 +0100

    Fixed login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"; 
 
 import './App.css';
-// import SignIn from "./components/SignIn/SignIn";
+import SignIn from "./components/SignIn/SignIn";
 import SignUp from "./components/SignUp/SignUp";
 import Friends from "./components/Friends/Friends";
-import Profile from "./components/Profile/Profile";
 import Home from "./pages/Home";
+import Header from "./components/Header/Header";
 
 
 
@@ -13,11 +13,11 @@ function App() {
   return (
     <div className="app">
         <Router>
-          <Home />
+          <Header />
           <Routes>
             <Route path="/register" element={<SignUp />} />
-            {/* <Route path="/login" element={<SignIn />} /> */}
-            <Route path="/profile" element={<Profile />} />
+            <Route path="/login" element={<SignIn />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/friends" element={<Friends />} />
           </Routes>
         </Router>
--- src/components/Profile/Profile.js
+++
@@ -1,9 +0,0 @@
-const Profile = () => {
-  return (
-    <article>
-      <span> Here will be your profile</span>
-    </article>
-  )
-}
-
-export default Profile;
